af3bc7b71a9d13b213b017cbe498639f4662a575
Anjali <anjalisingh930260@gmail.com>
2022-08-30 12:32:43 +0530

    version1

---
+++ webdev/projects/grocerylist/script.js
@@ -0,0 +1,29 @@
+"use strict";
+const grocWarn = document.querySelector('.hide-groc-warn'),
+    quantWarn = document.querySelector('.hide-quant-warn'),
+    item = document.querySelector('.groc-input-box'),
+    quant = document.querySelector('.quant-input-box'),
+    addBtn = document.querySelector('.add-btn'),
+    reminder = document.querySelector('.reminder'),
+    addItem = document.querySelector('.list-of-items'),
+    info = document.querySelector('.info'),
+    addCart = document.querySelector('.list-of-cart'),
+    listDisplay = document.querySelector('.list-display');
+let grabItems = 0;
+let cartItems = 0;
+
+//add button event
+
+//read input from input fields
+
+
+
+//add items to grocery list
+
+//store in local storage
+
+
+//delete btn event listener  for grocery list items
+
+
+//move i
